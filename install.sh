49a64c12334c6ce18297e98ff0af9286cf211bac
Gbit <34913299+gbit-is@users.noreply.github.com>
2020-11-24 07:42:57 +0000

    adding pip upgrade step

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ apt-get update
 apt-get install ffmpeg -y 
 apt-get install libsndfile-dev -y 
 apt-get install python3-pip -y
+pip install --upgrade pip
 pip3 install spleeter
 chmod +x /mnt/c/splee/bin/*
 ln -s /mnt/c/splee/bin/wrapper.sh /usr/local/bin/spleet
